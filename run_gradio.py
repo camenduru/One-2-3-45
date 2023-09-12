d27b909e3680ecdd96a52706adbf21fc90942301
camenduru <54370274+camenduru@users.noreply.github.com>
2023-09-12 19:39:01 +0300

    test

--- run_gradio.py
+++ run_gradio.py
@@ -95,7 +95,8 @@ def reconstruct(exp_dir, output_format=".ply", device_idx=0, resolution=256):
 #     # second stage: 4 local views for each of the first-stage view, resulting in N*4=32 source view images.
 #     stage2_run(model_zero123, device, shape_dir, elev, scale=3, stage2_steps=50)
 
-def generate(models, predictor, img_path, device):
+def generate(models, predictor, img_path):
+    device = f"cuda:0"
     shape_id = img_path.split('/')[-1].split('.')[0]
     shape_dir = f"./exp/{shape_id}"
     os.makedirs(shape_dir, exist_ok=True)
@@ -127,11 +128,11 @@ block = gr.Blocks().queue()
 with block:
     with gr.Row():
         with gr.Column():
-            input_image = gr.Image(source='upload', type="filepath")
+            img_path = gr.Image(source='upload', type="filepath")
             run_button = gr.Button(label="Run")
         with gr.Column():
             mesh_path = gr.Textbox(label="Mesh Path")
             # mesh_output = gr.Model3D(clear_color=[0.0, 0.0, 0.0, 0.0], label="One-2-3-45's Textured Mesh", elem_id="model-3d-out")
-    run_button.click(fn=partial(generate, models, predictor), inputs=[input_image, device], outputs=[mesh_path])
+    run_button.click(fn=partial(generate, models, predictor), inputs=[img_path], outputs=[mesh_path])
 
 block.launch(share=True)
